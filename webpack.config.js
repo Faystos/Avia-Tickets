2222669547172e7bbc8dd1ab45ed50e5b40f8783
Vladimir <fayst_@bk.ru>
2020-12-21 20:47:26 +0300

    Добавил файл Readme.md с описанием проекта, а так же убрал комментарии в конфигурационном файле WebPack.

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,12 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 
-module.exports = {
-  // Итак,  чтобы вебпак начал свою работу, нужно указать главный (основной) файл, который будет включать в себя все другие необходимые файлы (модули).
+module.exports = {  
   entry: {
     polyfill: 'babel-polyfill',
     app: './js/index.js',
   },
-  // Также webpack рекомендует явно указывать, в какой директории находятся исходные файлы проекта (ресурсы). Для этого следует использовать свойство context:
+ 
   context: path.resolve(__dirname, 'src'),
   devServer: {
     publicPath: '/',
@@ -24,10 +23,7 @@ module.exports = {
     stats: 'minimal',
     hot: true,
   },
-  module: {
-    // Для того, чтобы трансформировать файл, используются специальные утилиты - загрузчики (loaders).
-    //Для любых настроек модуля вебпак используется поле module.
-    //Массив rules  внутри объекта module определяет список правил для загрузчиков.
+  module: {    
     rules: [
       {
         use: {
@@ -72,19 +68,14 @@ module.exports = {
         ],
       },
     ],
-  },
-  // Вебпак плагины используются для настройки процесса сборки.
-  //Например, плагин для минификации кода (во время сборки код подвергается очистке и минификации).
-  //Или плагин для сборки html страницы и css кода (скрипты вставляются в html, куски css собираются в один файл).
+  }, 
   plugins: [
     new MiniCssExtractPlugin({ filename: './style.css' }),
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
     new CleanWebpackPlugin()
-  ],
-  // Кроме entry, мы можем указать поле, куда (в какой файл) собирать конечный результат. Это свойство задаётся с помощью поля output.
-  //По умолчанию, весь результирующий код собирается в папку dist.
+  ],  
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[hash].js',
